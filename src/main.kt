707b74b9840227eda4febbf10999ba4606927672
dobro <dobromilovartem@gmail.com>
2025-09-13 20:37:29 +0700

    create new

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+class Human{
+    var name: String
+    var surname: String
+    var second_name: String
+    var age: Int
+    var speed: Int
+    var x: Double
+    var y: Double
+
+    constructor(_name: String, _surname: String, _second_name: String, _age: Int, _speed:Int, _x: Double, _y: Double) {
+        name = _name
+        surname = _surname
+        second_name = _second_name
+        age = _age
+        speed = _speed
+        x = _x
+        y = _y
+    }
+
+    fun move(){
+
+    }
+}
+
+
+fun main(){
+}
