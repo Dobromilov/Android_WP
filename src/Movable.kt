eeca51076453e5484848bc7abe8635532c992e0a
Dobromilov <dobromilovartem@gmail.com>
2025-09-25 22:42:19 +0700

    FIX Mutex + exploring Interfaces

---
+++ src/Movable.kt
@@ -0,0 +1,10 @@
+package Inheritance
+
+interface Movable {
+
+    var speed: Double
+    var x: Double
+    var y: Double
+
+    fun move()
+}
--- src/main.kt
+++ src/main.kt
@@ -7,17 +7,17 @@ import kotlinx.coroutines.sync.withLock
 fun main() = runBlocking {
     val mutex = Mutex()
     val humans = listOf(
-        Human("Влад", "Погорелов", "Батькович", 9.0, 30),
-        Human("Роман", "Петров", "Юрьевич", 9.5, 25),
-        Human("Мария", "Иванова", "Сергеевна", 8.2, 28),
-        Driver("Алексей", "Сидоров", "Викторович", 8.0, 35, "Toyota")
+        Human("Влад", "Погорелов", "Батькович", 9.0),
+        Human("Роман", "Петров", "Юрьевич", 9.5),
+        Human("Мария", "Иванова", "Сергеевна", 8.2,),
+        Driver("Алексей", "Сидоров", "Викторович", 8.0, "Toyota")
     )
 
     val jobs = humans.mapIndexed { index, human ->
         launch(Dispatchers.Default) {
             for (time in 1..5) {
-                print("Время: ${time}c")
                 mutex.withLock {
+                    print("Время: ${time}c ")
                     print("${index + 1}): ")
                     human.move()
                 }
